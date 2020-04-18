61ec8be8a0deb5848f95953e878c7750d0371996
Carlos Diosdado <carlos.dddo@gmail.com>
2020-04-17 19:09:50 -0500

    more fixes

--- samples/recorrido360/data.js
+++ samples/recorrido360/data.js
@@ -31,7 +31,7 @@ var APP_DATA = {
       "linkHotspots": [
         {
           "yaw": 1.5707963267948966,
-          "pitch": 0,
+          "pitch": -0.261799387799149,
           "rotation": 0,
           "target": "1-salon"
         }
